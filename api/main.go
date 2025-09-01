8a9c614b39e2c94d01a830b420d6c35a46d897f0
v420v <ibuki420v@gmail.com>
2025-09-01 19:37:31 +0900

    add: graceful shutdown

--- api/main.go
+++ api/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/ryutaKimu/go_todo/api/router"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	r := router.Router()
-	log.Println("Server starting on :8000")
-	if err := http.ListenAndServe(":8000", r); err != nil {
-		log.Fatal("Server failed to start:", err)
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: r,
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		log.Printf("server starting at port %s", srv.Addr)
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("server error: %v", err)
+		}
+		log.Println("server stopped serving new connections")
+	}()
+
+	<-ctx.Done()
+	log.Println("received signal interrupt")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("server shutdown error: %v", err)
+	}
+
+	log.Println("server graceful shutdown complete")
 }
