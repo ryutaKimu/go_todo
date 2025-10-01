5d3c98f2392110782c85bc652db78b0413ac133f
ryutaKimu <rekkoilove@softbank.ne.jp>
2025-10-02 04:23:17 +0900

    add: 中間テーブル実装

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -3,5 +3,6 @@ package model
 type Todo struct {
 	Id          int    `json:"id"`
 	Title       string `json:"title"`
+	TagIds      []int  `json:"tag_ids"`
 	IsCompleted bool   `json:"is_completed"`
 }
